132a4ef0b3f391a4d38a2cb9cf5d0e8e9ca702c6
Ruslan Alimov <2001.ruslan.alimov@gmail.com>
2022-05-17 00:00:09 +0300

    Initial commit

---
+++ conf/config.py
@@ -0,0 +1,15 @@
+"""
+Main configuration file with final variable
+"""
+
+
+# Database info
+HOST = "localhost"
+PORT = 3306
+USERNAME = "root"
+PASSWORD = "12345"
+DATABASE_NAME = "tax_office"
+
+
+# Configuration file path
+CONFIG_FILE_PATH = r"./conf/userConfig.ini"
